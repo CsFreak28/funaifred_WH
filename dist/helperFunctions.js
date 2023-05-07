7d4586741eb5019a45e39f47bc034ee126fa2c13
CsFreak28 <uwagideon092@gmail.com>
2023-05-07 10:34:35 -0700

    create_textIsAGreeting_function

--- dist/helperFunctions.js
+++ dist/helperFunctions.js
@@ -1,4 +1,31 @@
+import { Configuration, OpenAIApi } from "openai";
+//setup openai configuration
+const openai = new OpenAIApi(new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+}));
 export function textIsAGreeting(text) {
+    let textIsAGreeting = "No";
+    openai
+        .createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+            {
+                role: "user",
+                content: `is ${text} a greeting, answer only yes or no`,
+            },
+        ],
+    })
+        .then((res) => {
+        var _a;
+        if (res !== undefined) {
+            textIsAGreeting = (_a = res.data.choices[0].message) === null || _a === void 0 ? void 0 : _a.content;
+            console.log(textIsAGreeting);
+            // msgToSend = res.data.choices[0].message.content;
+        }
+    })
+        .catch((e) => {
+        console.log("there was an error when processing request");
+    });
     return false;
 }
 export function getConversation(phoneNumber, mapOfConversations) { }
--- src/helperFunctions.ts
+++ src/helperFunctions.ts
@@ -1,4 +1,33 @@
+import { AxiosResponse } from "axios";
+import { Configuration, OpenAIApi } from "openai";
+//setup openai configuration
+const openai = new OpenAIApi(
+  new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  })
+);
 export function textIsAGreeting(text: string): boolean {
+  let textIsAGreeting: string | undefined = "No";
+  openai
+    .createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `is ${text} a greeting, answer only yes or no`,
+        },
+      ],
+    })
+    .then((res) => {
+      if (res !== undefined) {
+        textIsAGreeting = res.data.choices[0].message?.content;
+        console.log(textIsAGreeting);
+        // msgToSend = res.data.choices[0].message.content;
+      }
+    })
+    .catch((e) => {
+      console.log("there was an error when processing request");
+    });
   return false;
 }
 export function getConversation(phoneNumber: string, mapOfConversations: []) {}
